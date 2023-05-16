442630f5e565f8e10c6ae8610a6fd302924b053c
Nikolas <subedikiran678@gmail.com>
2023-05-16 23:19:47 +0545

    ssff

--- ship.py
+++ ship.py
@@ -8,7 +8,7 @@ class Ship():
 
         #Load the ship image and get its rect.
         self.image = pygame.image.load('ship.png')
-        self.react = self.imagine.get_rect()
+        self.react = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
         #Start each new ship at the bottom center of the screen.

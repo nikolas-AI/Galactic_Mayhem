874a638fcfb2d08de58f4907e79e9a7ed90656e5
Nikolas <subedikiran678@gmail.com>
2023-05-17 12:48:12 +0545

    hhh

--- setttings.py
+++ setttings.py
@@ -6,4 +6,4 @@ class Settings():
         #Screen settings
         self.screen_width = 1000
         self.screen_height = 700
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (25, 25, 10)

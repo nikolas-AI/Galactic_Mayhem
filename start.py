67dbee64f683995188dce9907fe2f8f5d05642c4
Nikolas <subedikiran678@gmail.com>
2023-05-18 00:09:12 +0545

    sssd

--- start.py
+++ start.py
@@ -22,11 +22,11 @@ def run_game():
     #Make a ship, a group to store bullets in, and a group of aliens.
     ship = Ship(ai_settings, screen )
     bullets = Group()
-    alien = Alien(ai_settings, screen)
-    # aliens = Group()
+    # alien = Alien(ai_settings, screen)
+    aliens = Group()
 
-    #Create the fleet of aliens.
-    # gf.create_fleet(ai_settings, screen, aliens)
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
 
     #Start the main loop for the game
     while True:

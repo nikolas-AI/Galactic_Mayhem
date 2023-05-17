f9d71a10bd4fc07f77dc3fd4b43172dda14381dd
Nikolas <subedikiran678@gmail.com>
2023-05-17 12:30:14 +0545

    images folder

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship():
         self.screen = screen
 
         #Load the ship image and get its rect.
-        self.image = pygame.image.load('ship.png')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 

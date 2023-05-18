de1f6fedfb21774883f720cdee6377e47842b732
Nikolas <subedikiran678@gmail.com>
2023-05-18 15:55:22 +0545

    alien's speed factor

--- setttings.py
+++ setttings.py
@@ -17,3 +17,7 @@ class Settings():
         self.bullet_height =15
         self.bullet_color = 'Red'
         self.bullets_allowed = 3
+
+        #Alien settings
+        self.alien_speed_factor = 1
+        

5067d574097a11b886087f5ee37bdf75794733aa
Nikolas <subedikiran678@gmail.com>
2023-05-18 16:18:56 +0545

    changing fleet direction

--- game_functions.py
+++ game_functions.py
@@ -72,7 +72,11 @@ def check_fleet_edges(ai_settings, aliens):
                change_fleet_direction(ai_settings, aliens)
                break
 
-
+def change_fleet_direction(ai_settings, aliens):
+     """Drop the entire fleet and change the fleet's direction."""
+     for alien in aliens.sprites():
+          alien.rect.y += ai_settings.fleet_drop_speed
+     ai_settings.fleet_diretion *= -1
 
 
 

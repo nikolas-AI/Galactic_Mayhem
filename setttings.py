438e912dfc17108eac6caaff68abc99f79fdbd61
Nikolas <subedikiran678@gmail.com>
2023-05-20 00:01:04 +0545

    perfection

--- setttings.py
+++ setttings.py
@@ -5,7 +5,7 @@ class Settings():
         """Initialize the game's settings."""
         #Screen settings
         self.screen_width = 1000
-        self.screen_height = 800
+        self.screen_height = 700
         self.bg_color = 'steelblue'
 
         #Ship settings

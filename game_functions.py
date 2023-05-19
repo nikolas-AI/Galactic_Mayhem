bc2b6896c52e67ca2e4baa09b7214014fa303016
Nikolas <subedikiran678@gmail.com>
2023-05-19 23:35:14 +0545

    ship_hit

--- game_functions.py
+++ game_functions.py
@@ -1,4 +1,5 @@
 import sys
+from time import sleep
 
 import pygame
 
@@ -90,14 +91,30 @@ def change_fleet_direction(ai_settings, aliens):
           alien.rect.y += ai_settings.fleet_drop_speed
      ai_settings.fleet_direction *= -1
 
-def update_aliens(ai_settings, ship, aliens):
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+     """Respond to ship being hit by alien."""
+     #Decrement ships left.
+     stats.ship_left -= 1
+
+     #Empty the list of aliens and bullets.
+     aliens.empty()
+     bullets.empty()
+
+     #Create a new fleet and center the ship.
+     create_fleet(ai_settings, screen, ship, aliens)
+     ship.center_ship()
+
+     #Pause
+     sleep(0.5)
+
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
      """Check if the fleet is at an edge, and then update the postions of all aliens in the fleet."""
      check_fleet_edges(ai_settings, aliens)
      aliens.update()
 
      #Look for the aluen_ship collections.
      if pygame.sprite.spritecollideany(ship, aliens):
-          print("Ship hit! !!")
+          ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
     """Fire a bullet if limit not reached yet."""

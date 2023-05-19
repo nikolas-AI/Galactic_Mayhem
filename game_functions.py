e4160332c544d9ac703104afc9a1981d6bcffd57
Nikolas <subedikiran678@gmail.com>
2023-05-19 23:49:10 +0545

    checkin aliens at bottom

--- game_functions.py
+++ game_functions.py
@@ -107,10 +107,21 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
      #Pause
      sleep(0.5)
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+     """Check if any aliens have reached the bottom of the screen."""
+     screen_rect = screen.get_rect()
+     for alien in aliens.sprites():
+          if alien.rect.bottom >= screen_rect.bottom:
+               #Treate this same as if the ship got hit.
+               ship_hit(ai_settings, stats, ship, aliens, bullets)
+
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
      """Check if the fleet is at an edge, and then update the postions of all aliens in the fleet."""
      check_fleet_edges(ai_settings, aliens)
      aliens.update()
+
+     #Look for aliens hitting the bottom of the screen.
+     check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
 
      #Look for the aluen_ship collections.
      if pygame.sprite.spritecollideany(ship, aliens):

c1b3fda02ba3838a546642ef4ea316f94d432d20
Nikolas <subedikiran678@gmail.com>
2023-05-19 23:58:06 +0545

    ddd

--- start.py
+++ start.py
@@ -13,11 +13,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 
-    bg = pygame.image.load('images/bg.jfif')
-    bg1 = pygame.image.load('images/bg.jfif')
-    
-    pygame.display.set_caption("The Invaison")
-
+    bg = pyg
     #Create an instance to store game statistics.
     stats = GameStats(ai_settings)
 
@@ -33,7 +29,7 @@ def run_game():
     #Start the main loop for the game
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        
+
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)

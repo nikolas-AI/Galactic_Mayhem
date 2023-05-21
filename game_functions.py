0ca31efcfca14b9c7a83ad7d27c1d0fe78f4b4b1
Nikolas <subedikiran678@gmail.com>
2023-05-21 23:59:36 +0545

    ffgggg

--- game_functions.py
+++ game_functions.py
@@ -29,14 +29,7 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
         if event.type == pygame.QUIT:
             sys.exit()
 
-        elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ai_settings, screen, ship, bullets)
-
-        elif event.type == pygame.KEYUP:
-            check_keyup_events(event, ship)
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-             mouse_X, mouse_y = pygame.mouse.get_pos()
+        elif event.tyme.mouse.get_pos()
              check_play_buttons(stats, play_button, mouse_X, mouse_y)
 
            

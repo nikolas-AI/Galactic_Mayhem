fa8f60ee57f517263ba008a1cbf1e8e27d751587
Nikolas <subedikiran678@gmail.com>
2023-05-21 23:50:10 +0545

    draw button

--- button.py
+++ button.py
@@ -25,4 +25,7 @@ class Button():
         self.msg_image =self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
+
+    def draw_buttons(self):
+        #Draw blank button and then draw message.
         

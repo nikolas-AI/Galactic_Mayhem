fdcce93dc62798422fa27c9c7390e53be3777637
Nikolas <subedikiran678@gmail.com>
2023-05-21 23:38:28 +0545

    setting properties of button

--- button.py
+++ button.py
@@ -7,5 +7,11 @@ class Button():
         self.screen = screen
         self.screen_rect = screen.gt_rect()
 
-      
+        #Set the dimensions and properties of the button.
+        self.width, self.heigth1 = 200, 50
+        self.button_color =(0, 2555, 0)
+        self.text_color =(255, 255, 255)
+        self.font = pygame.font.SysFont(None, 48)
+
+       
         

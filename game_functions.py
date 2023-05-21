cd7222a20bf92e207fb094754d4012ab1901fb43
Nikolas <subedikiran678@gmail.com>
2023-05-22 00:01:16 +0545

    play buttons

--- game_functions.py
+++ game_functions.py
@@ -36,8 +36,8 @@ def check_events(ai_settings, screen, stats, play_button, ship, bullets):
             check_keyup_events(event, ship)
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
-             mouse_X, mouse_y = pygame.mouse.get_pos()
-             check_play_buttons(stats, play_button, mouse_X, mouse_y)
+             mouse_x, mouse_y = pygame.mouse.get_pos()
+             check_play_buttons(stats, play_button, mouse_x, mouse_y)
 
            
              

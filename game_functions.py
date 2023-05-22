1eb07f9e0b0eb8a5f3cfabea879633367e75fbc4
Nikolas <subedikiran678@gmail.com>
2023-05-22 14:02:37 +0545

    deactivationg play button

--- game_functions.py
+++ game_functions.py
@@ -39,9 +39,10 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
              mouse_x, mouse_y = pygame.mouse.get_pos()
              check_play_buttons(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y)
 
-def check_play_buttons(ai_settings, screen, stats, play_buttons, ship, aliens, bullets, mouse_x, mouse_y):
+def check_play_buttons(ai_settings, screen, stats, play_button, ship, aliens, bullets, mouse_x, mouse_y):
      """Start a new game only when the player clicks Play."""
-     if play_buttons.rect.collidepoint(mouse_x, mouse_y):
+     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
+     if button_clicked and not stats.game_active:
           #Reset the game statistics.
           stats.reset_stats()
           stats.game_active = True    

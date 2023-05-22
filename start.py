1c511eaf7d26ea5ac0b17937f6a299513fe66e35
Nikolas <subedikiran678@gmail.com>
2023-05-22 13:53:37 +0545

    sssdd

--- start.py
+++ start.py
@@ -28,7 +28,7 @@ def run_game():
     #Make a ship, a group to store bullets in, and a group of aliens.
     ship = Ship(ai_settings, screen )
     bullets = Group()
-    # alien = Alien(ai_settings, screen)
+    # alien = Alien(ai_settings, screen) 
     aliens = Group()
 
     # Create the fleet of aliens.
@@ -36,7 +36,7 @@ def run_game():
 
     #Start the main loop for the game
     while True:
-        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
         
         if stats.game_active:
             ship.update()

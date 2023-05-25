5d65b2fc5590de09a10e7d45d582fb7845914f0a
Nikolas <subedikiran678@gmail.com>
2023-05-25 12:09:58 +0545

    2nd bullet

--- alien_bullet.py
+++ alien_bullet.py
@@ -10,12 +10,14 @@ class Alien_Bullet(Sprite):
 
         self.screen = screen
 
-        #Create a bullet at (185,1700) 
+        #Create 2 bullet at (185,170) and (585, 170) 
         self.rect = pygame.Rect(185, 170, ai_settings.bullet_width, ai_settings.bullet_height)
+        self.rect2 = pygame.Rect(585, 170, ai_settings.bullet_width, ai_settings.bullet_height)
         
 
         #Store the bullet's position as a decimal value.
-        self.y =float(self.rect.y)
+        self.y = float(self.rect.y)
+        self.y2 = float(self.rect2.y)
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
@@ -24,10 +26,13 @@ class Alien_Bullet(Sprite):
         """Move the bullet up in the screen."""
         #Update the decimal position of the bullet.
         self.y += self.speed_factor
+        self.y2 += self.speed_factor
         #Update the rect position.
         self.rect.y = self.y
+        self.rect2.y = self.y2
 
     def draw_alien_bullets(self):
         """Draw the bullet to the screen."""
         pygame.draw.rect(self.screen, self.color, self.rect)
+        pygame.draw.rect(self.screen, self.color, self.rect2)
 
